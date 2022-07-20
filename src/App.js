9adf4b5365fa7afd140f0a7ac1120a87a8f62587
wilfredoo <cacao_de_verdad@protonmail.com>
2022-07-20 17:53:34 +0200

    Ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import autos from "./autos";
 
 function App() {
+  const [autoImages, setAutoImages] = useState(null)
+  const [errorPage, setErroPage] = useState(true)
+
+
+
+  useEffect(() => {
+    console.log("autos", autos)
+    setAutoImages(autos)
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {!errorPage ?
+    <>
+        <p>Car Images</p>
+        {autoImages && autoImages.map(auto => <img className='auto' src={auto} />
+
+        )}
+      </>
+      : <p className='error'>Error 416</p>
+}
+      </div> 
   );
 }
 
